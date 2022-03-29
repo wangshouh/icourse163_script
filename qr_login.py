ce1355ff96434221f7f8373e6fe3eab9dfb6f43a
Wong Shouhao <wongshouhao@outlook.com>
2022-03-29 20:02:41 +0800

    Get scan qr result.

--- qr_login.py
+++ qr_login.py
@@ -3,6 +3,7 @@ import json
 import time
 import re
 
+
 def get_qr_info(s):
     url = "https://www.icourse163.org/logonByQRCode/code.do?width=182&height=182"
 
@@ -17,6 +18,7 @@ def get_qr_info(s):
     response_json = json.loads(response.text)
     return response_json['result']
 
+
 def get_qr_img(qr_url):
     url = qr_url
 
@@ -30,3 +32,16 @@ def get_qr_img(qr_url):
         img_data = response.content
         f.write(img_data)
 
+
+def get_qr_result(s, pollkey):
+    url = f"https://www.icourse163.org/logonByQRCode/poll.do?pollKey={pollkey}"
+
+    headers = {
+        'Host': 'www.icourse163.org',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+    }
+
+    response = s.get(url, headers=headers)
+    response_json = json.loads(response.text)
+
+    return response_json['result']
