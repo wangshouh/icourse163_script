c05b0c4b6c4c3ca054cb7d8e17da4988a4021dbc
Wong Shouhao <wongshouhao@outlook.com>
2022-03-29 19:47:50 +0800

    Finish like function.

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import requests
 import json
 from requests.structures import CaseInsensitiveDict
 import urllib.parse
+
+def like(s, pid):
+    url = "https://www.icourse163.org/dwr/call/plaincall/MocForumBean.markVote.dwr"
+
+    data = f"callCount=1\nscriptSessionId=$scriptSessionId190\nhttpSessionId=9a3b757176934c82b1874ca5bda0eab7\nc0-scriptName=MocForumBean\nc0-methodName=markVote\nc0-id=0\nc0-param0=number:{pid}\nc0-param1=number:{pid}\nc0-param2=number:1\nc0-param3=number:1\nc0-param4=number:0\nbatchId=1648477205532"
+
+    resp = s.post(url, data=data)
+
+    print(resp.text)
