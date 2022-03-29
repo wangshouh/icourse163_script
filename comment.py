138d47905b4cc2b59e34772eb4587ae809954af4
Wong Shouhao <wongshouhao@outlook.com>
2022-03-29 21:26:52 +0800

    Add comments.

--- comment.py
+++ comment.py
@@ -3,7 +3,9 @@ import requests
 
 
 def get_comments_list(s, pid):
-
+    '''
+    获取评论区第二页评论
+    '''
     url = "https://www.icourse163.org/dwr/call/plaincall/PostBean.getPaginationReplys.dwr"
 
     payload = f"callCount=1\nscriptSessionId=$scriptSessionId190\nc0-scriptName=PostBean\nc0-methodName=getPaginationReplys\nc0-id=0\nc0-param0=number:{pid}\nc0-param1=number:2\nc0-param2=number:2\nbatchId=1648445424731"
