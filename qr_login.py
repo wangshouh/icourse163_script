f385628e1220c4c08c48a13f7ce5893354506096
Wong Shouhao <wongshouhao@outlook.com>
2022-03-29 20:00:28 +0800

    Get qr url and pollkey.

--- qr_login.py
+++ qr_login.py
@@ -2,3 +2,17 @@ import requests
 import json
 import time
 import re
+
+def get_qr_info(s):
+    url = "https://www.icourse163.org/logonByQRCode/code.do?width=182&height=182"
+
+    payload = {}
+    headers = {
+        'Host': 'www.icourse163.org',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+    }
+
+    response = s.request("GET", url, headers=headers, data=payload)
+
+    response_json = json.loads(response.text)
+    return response_json['result']
